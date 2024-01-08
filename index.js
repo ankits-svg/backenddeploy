c728f698940c5bec4c3b2f10eb8c9a7aa37fd5f0
Ankit Sharma <ankits745@gmail.com>
2024-01-09 00:05:45 +0530

    changing path as per linux

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { Storage } = require("@google-cloud/storage");
 const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } =
   process.env;
 
-// CONFIGURE CLOUDINARY
+
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
   api_key: CLOUDINARY_API_KEY,
@@ -18,52 +18,30 @@ cloudinary.config({
 });
 const cors = require("cors");
 
-let projectId = process.env.projectId; // Get this from Google Cloud
+let projectId = process.env.projectId; 
 
 let keyFilename = process.env.keyFilename;
 
-// keyFilename = path.normalize(keyFilename);
-// Use path.join to construct the file path
-// keyFilename = path.join(__dirname, keyFilename);
-// Convert the Windows path to a Linux path
-// keyFilename = path.posix.join("/mnt/c", keyFilename.replace(/\\/g, "/"));
-// Convert the Windows path to an absolute path
-// Convert the Windows path to a Linux path
-// keyFilename = path.posix.join("/home/hp/Downloads/", "axial-engine-410216-0df0ce8f2e41.json");
-  // Convert the Windows path to a Linux path
-  // keyFilename = keyFilename.replace(/\\/g, "/");
-  // keyFilename = keyFilename.replace("\\", "/")
-  // console.log("keyfilename:",keyFilename)
-//   keyFilename = path.join(__dirname, keyFilename);
-// keyFilename = path.normalize(keyFilename);
-
-// // Handle Windows drive letter if present
-// keyFilename = keyFilename.replace(/^([A-Z]):\\/, '');
-
-// Get this from Google Cloud -> Credentials -> Service Accounts
 const storage = new Storage({
   projectId,
   keyFilename,
 });
-const bucketName = process.env.bucket; // Replace with your actual bucket name
-// console.log("bucketname:",bucketName)  // 1
+const bucketName = process.env.bucket; 
+
 
 port = process.env.port || 2200;
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(cors({ origin: "*" }));
 
-//https://front-nuqxdx86l-ankits-projects-b7dffc9e.vercel.app
-// CORS Configuration
 const corsConfig = [
   {
     maxAgeSeconds: 3600,
     method: ["GET", "PATCH"],
-    origin: ["*"], //   Actual domain
+    origin: ["*"], 
     responseHeader: ["*"],
   },
 ];
 
-// Set CORS Configuration for the bucket
 async function configureBucketCors() {
   await storage.bucket(bucketName).setCorsConfiguration(corsConfig);
   console.log(`Bucket ${bucketName} was updated with CORS configuration`);
@@ -113,41 +91,30 @@ app.patch("/update/:id", async (req, res) => {
   const { id } = req.params;
 
   const { img } = req.body;
-  // console.log("iasdadad:",img)
+ 
   const rank = await RankModel.findById(id);
-  // console.log(rank)
-  //New code for cloudinary
+
   try {
-    // DEFINE UPLOAD OPTIONS
     const options = {
       public_id: `${id}`,
       folder: `${rank.course}`,
       unique_filename: true,
       use_filename: true,
     };
-    // console.log("options:",options)
-    // UPLOAD IMAGE TO CLOUDINARY
+   
     const response = await cloudinary.uploader.upload(img, options);
-    // console.log("response:",response)
-    // RETURN UPLOADED IMAGE DATA
+    
     const uploadedImage = response.url;
 
     await RankModel.findByIdAndUpdate(id, { img: uploadedImage });
-    // console.log("rankss:",rankss)
-
-    // console.log("uploadedImage:",uploadedImage)
-    // RETURN COMPLETE RESPONSE
     return res.status(200).json({ uploadedImage, response });
 
-    // return res.send({"msg":"cloudinary data","data":rankss})
-    // CATCH ERROR
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
-  //New code for cloudinary
+ 
 });
 
-// Google cloud storage route
 app.patch("/updates/:id", async (req, res) => {
   const { id } = req.params;
   const { img } = req.body;
@@ -156,60 +123,51 @@ app.patch("/updates/:id", async (req, res) => {
     const response = await uploadImageToGoogleCloudStorage(id, img);
     console.log("response:", response);
 
-    // Update the database with the new image URL
     await RankModel.findByIdAndUpdate(id, { img: response.imageUrl });
-    // await RankModel.findByIdAndUpdate(id, { img: originalLink });
+
 
     return res.status(200).json({ uploadedImage: response.imageUrl, response });
-    // return res.status(200).json({ uploadedImage: newLink, response });
+
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 });
 
 async function uploadImageToGoogleCloudStorage(id, image) {
-  const fileName = `${id}.png`; // Adjust the file name as needed   //1
-  const filePath = path.join(__dirname, "temp", fileName); //1
+  const fileName = `${id}.png`;
+  const filePath = path.join(__dirname, "temp", fileName);
 
-  // Save the image to a temporary file
+
   await saveImageToFile(image, filePath).catch((error) => {
     throw new Error(`Failed to save image to file: ${error.message}`);
   });
 
-  // Upload the image to Google Cloud Storage
   const bucket = storage.bucket(bucketName);
-  // console.log("bucket:",bucket) //1
+
   const options = {
     destination: `Certificate/${fileName}`,
   };
 
-  // console.log("option:", options); //1
-
-  //Code is breaking here
 
   await bucket.upload(filePath, options);
 
-  // // Get the public URL of the uploaded image
   const imageUrl = `https://storage.googleapis.com/${bucketName}/Certificate/${fileName}`;
 
-  // Return the image URL
   return { imageUrl };
 }
 
 async function saveImageToFile(image, filePath) {
   return new Promise((resolve, reject) => {
     const base64Image = image.split(";base64,").pop();
-    // console.log("base64Image:",base64Image)
+   
     require("fs").writeFile(
       filePath,
       base64Image,
       { encoding: "base64" },
       (err) => {
         if (err) {
-          // console.log("errin fs:", err);
           reject(err);
         } else {
-          // console.log("resolve");
           resolve();
         }
       }
@@ -217,7 +175,6 @@ async function saveImageToFile(image, filePath) {
   });
 }
 
-// Express route to get an image by its ID
 app.get("/images/:id", async (req, res) => {
   try {
     const image = await RankModel.findById(req.params.id);
